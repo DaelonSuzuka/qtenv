037c25becb8dab38af7d27dce1452186dd5dcde9
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-01-21 14:39:46 -0500

    Added BaseApplication

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from .qt import *
 from .utils import *
 from .layouts import *
 from .widgets import *
+from .base_application import BaseApplication
 from .base_window import BaseMainWindow
 from .toolbar import BaseToolbar
 from .splitter import CSplitter, CPersistentSplitter
